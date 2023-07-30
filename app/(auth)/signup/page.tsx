0839ac18d2ad407f37f2eda0c2c6166a3b7de3d4
Mahamed Mahad <55987068+mahamedmahad@users.noreply.github.com>
2023-07-31 00:28:44 +0200

    refactor: update components under learn folder

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
-export default function Signup() {
+const Signup = () => {
   return (
     <div className="min-h-screen">
       <div className="container mx-auto">
-        <div className="m-40 flex flex-col items-center justify-center sm:flex-row">
+        <div className="flex flex-col items-center justify-center m-40 sm:flex-row">
           <div className="text-center sm:w-1/2">
             <h3 className="text-3xl font-bold">Create your profile</h3>
             <div className="flex flex-col items-center justify-center mt-8 space-y-4">
@@ -17,3 +17,5 @@ export default function Signup() {
     </div>
   );
 }
+
+export default Signup;
--- components/learn/Lessons.tsx
+++ components/learn/Lessons.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
-const LessonCard = ({ title, subtitle, progress }) => {
+
+type LessonCardProps = {
+  title: string,
+  subtitle: string,
+  progress: number
+}
+
+
+const LessonCard = ({ title, subtitle, progress }: LessonCardProps) => {
   return (
     <div className={`bg-[#86cc05] p-4 rounded-lg shadow-[0px_4px_0px_0px_#38a169] cursor-pointer hover:bg-[#86cc05b3] transition-colors duration-200`}>
       <h3 className={`text-2xl font-black text-white`}>{title}</h3>
@@ -45,7 +53,7 @@ function Lessons() {
   ];
 
   return (
-    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-2">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2">
       {lessonsData.map((lesson, index) => (
         <LessonCard key={index} {...lesson} />
       ))}
