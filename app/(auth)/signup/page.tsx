5c91868e8fad76b9c064d9000816c00f16d56c07
jamaalcoder <kabarojamaal@gmail.com>
2023-07-31 05:45:44 +0100

    ui fixed - responsive devices and custom tailwind breakpoints

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 const Signup = () => {
   return (
-    <div className="min-h-screen">
+    <div className="">
       <div className="container mx-auto">
-        <div className="flex flex-col items-center justify-center m-40 sm:flex-row">
+        <div className="flex flex-col items-center justify-center sm:flex-row">
           <div className="text-center sm:w-1/2">
             <h3 className="text-3xl font-bold">Create your profile</h3>
             <div className="flex flex-col items-center justify-center mt-8 space-y-4">
-            <Link href="/learn" className="w-3/5 h-12 px-4 py-4 rounded-xl text-sm tracking-widest bg-blue-400 text-white font-bold hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2 shadow-[0px_4px_0px_0px_#4299E1]">
+            <Link href="/learn" className="w-3/5  px-4 py-4 rounded-xl text-sm tracking-widest bg-blue-400 text-white font-bold hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2 shadow-[0px_4px_0px_0px_#4299E1]">
                 CREATE ACCOUNT{" "}
               </Link>
             </div>
--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -3,14 +3,14 @@ import {  SideBar, Lessons} from '@/components'
 const Learn = () => {
   return (
    
-        <div className="flex flex-row min-h-screen p-20 space-18">
-            <div className="w-1/5 pr-6 border-r-2 border-blue-100">
-                <SideBar />
-            </div>
-            <div className="flex-grow pl-10">
-                <Lessons />
-            </div>
-        </div>
+    <div className=" flex flex-row p-20 c-xl:p-5 space-18">
+    <div className="pr-1 border-r-2 border-blue-100">
+        <SideBar />
+    </div>
+    <div className="flex-grow pl-5">
+        <Lessons />
+    </div>
+</div>
   );
 }
 
