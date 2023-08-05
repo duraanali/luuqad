1968c81a0bb09d196ca21f8de80ee095b1f20f74
Duraan Ali <16447314+duraanali@users.noreply.github.com>
2023-08-05 03:42:46 -0500

    admin area changes

--- middleware.ts
+++ middleware.ts
@@ -8,16 +8,28 @@ export default async function middleware(req: NextRequest) {
   const role = token?.role
   const isAuthenticated = !!token
 
-  // if user is authenticated and is admin, allow access to "/learn" and "/admin"
+  // If the user is authenticated and is an admin, allow access to "/learn" and "/admin"
   if (isAuthenticated && role === "admin") {
-    if (req.nextUrl.pathname.startsWith("/admin")) {
+    if (
+      req.nextUrl.pathname.startsWith("/learn") ||
+      req.nextUrl.pathname.startsWith("/admin")
+    ) {
       return NextResponse.next()
     } else {
       return NextResponse.rewrite(`${origin}/admin`)
     }
   }
 
-  // if user is not authenticated, allow access to login and signup pages
+  // If the user is authenticated but not an admin, allow access to "/learn" only
+  if (isAuthenticated && role !== "admin") {
+    if (req.nextUrl.pathname.startsWith("/learn")) {
+      return NextResponse.next()
+    } else {
+      return NextResponse.rewrite(`${origin}/learn`)
+    }
+  }
+
+  // If the user is not authenticated, allow access to login and signup pages
   if (!isAuthenticated) {
     if (
       req.nextUrl.pathname.startsWith("/login") ||
@@ -25,11 +37,14 @@ export default async function middleware(req: NextRequest) {
       req.nextUrl.pathname.startsWith("/")
     ) {
       return NextResponse.next()
-    } 
+    } else {
+      return NextResponse.rewrite(`${origin}/login`)
+    }
   }
 
-  // if user is authenticated but not an admin, redirect to "/learn"
-  return NextResponse.rewrite(`${origin}/learn`)
+  // For any other case (e.g., user authenticated as admin but trying to access "/" or "/login" or "/signup"),
+  // redirect to "/learn" if logged in, or "/login" if not logged in.
+  return NextResponse.rewrite(`${origin}/learn`);
 }
 
 export const config = {
