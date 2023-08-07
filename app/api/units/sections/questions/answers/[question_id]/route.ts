5e673adf5aaf3ad9ad0f2fd53ccd219fa26430fd
Duraan Ali <16447314+duraanali@users.noreply.github.com>
2023-08-07 00:50:06 -0500

    Redux Toolkit can now show questions and answers

--- app/api/units/sections/questions/answers/[question_id]/route.ts
+++ app/api/units/sections/questions/answers/[question_id]/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server"
+import {NextRequest, NextResponse } from "next/server"
 import { options } from "@auth/[...nextauth]/options"
 import { getServerSession } from "next-auth/next"
 
