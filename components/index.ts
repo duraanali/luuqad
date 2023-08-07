56dfb7d41061bafd575a6b850962faff87960d39
Yasin Elabe <yasinelabe@gmail.com>
2023-08-07 22:05:33 +0300

    added email templates

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { default as Lessons } from "./learn/Lessons"
 export { default as SideBar } from "./learn/SideBar"
 export { default as Navbar } from "./navbar/Navbar"
+export {default as ForgetPassword } from "./email/ForgetPassword"
