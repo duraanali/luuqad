4f903378858fdcd20034143f1ed38b21528235b6
jamaalcoder <kabarojamaal@gmail.com>
2023-08-10 20:49:08 +0100

    i comment all the blocks for future use and removed some css

--- app/(2 Authenticated)/profile/page.tsx
+++ app/(2 Authenticated)/profile/page.tsx
@@ -2,6 +2,7 @@
 import { BottomBar } from "@/components/learn/BottomBar"
 import { LeftBar } from "@/components/learn/LeftBar"
 import { RightBar } from "@/components/learn/RightBar"
+import ProfileSettings from "@/components/profile/ProfileSettings"
 import ProfileUser from "@/components/profile/ProfileUser"
 import { NextPage } from "next"
 
@@ -10,19 +11,33 @@ type Props = {}
 const Profile: NextPage = (props: Props) => {
   return (
     <>
+      {/* TopBar-Container Here Start */}
       {/* <TopBar backgroundColor='bg-[#58cc02]' borderColor='border-[#46a302]' /> */}
-      <LeftBar selectedTab='Profile' />
+      <ProfileSettings />
+      {/* TopBar-Container Here End */}
 
-      <div className='main-right flex justify-center gap-3 pt-14 sm:p-6 sm:pt-6 md:ml-24 c-max-tm:w-[100%] c-max-td:w-[820px]   c-min-lg:ml-64 c-min-lg:gap-12'>
-        <div className=' main-middle-left flex max-w-[592px] grow flex-col'>
+      {/* LeftBar-Container Here Start */}
+      <LeftBar selectedTab='Profile' />
+      {/* LeftBar-Container Here End */}
+
+      {/* Main-Right-Middle-Container Here Start*/}
+
+      <div className='main-right-middle-container flex justify-center gap-12  sm:p-6 sm:pt-6 md:ml-24 c-max-tm:w-[100%] c-max-td:w-[820px]   c-min-lg:ml-64'>
+        {/* Main-middle-Left Scrolling Here Start */}
+        <div className=' main-middle-left flex max-w-[592px] grow flex-col c-sm:p-4 c-sm:mb-20'>
           <ProfileUser />
         </div>
+        {/* Main-middle-Left Scrolling Here End */}
+
+        {/* Main-middle-Right Sticky Here Start */}
         <RightBar />
+        {/* Main-middle-Right Sticky Here End */}
       </div>
+      {/* Main-Right-Middle-Container Here End */}
 
-      <div className='pt-[90px]'></div>
-
+      {/* BottomBar-Container Here Start */}
       <BottomBar selectedTab='Profile' />
+      {/* BottomBar-Container Here End */}
     </>
   )
 }
