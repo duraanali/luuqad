3d0a712c5fb7ed01ec1eb541b6f26ababea8a636
Duraan Ali <16447314+duraanali@users.noreply.github.com>
2023-08-10 21:43:27 -0500

    Profile shows accurate info now

--- README.md
+++ README.md
@@ -39,6 +39,16 @@ git clone https://github.com/{username}/luuqad.git
 npm install
 ```
 
+- Start the project:
+
+```bash
+npm run dev
+```
+
+- You also need to create .env.local file in the root directory of the project
+- Go to our Whatsapp group and ask the content for .env.local
+
+
 ## Code of Conduct
 
 Please read and adhere to our [Code of Conduct](docs/CODE_OF_CONDUCT.md) to ensure a respectful and inclusive community for everyone.
--- app/(2 Authenticated)/learn/profile/page.tsx
+++ app/(2 Authenticated)/learn/profile/page.tsx
@@ -2,12 +2,15 @@
 import { BottomBar } from "@/components/learn/BottomBar"
 import { LeftBar } from "@/components/learn/LeftBar"
 import { RightBar } from "@/components/learn/RightBar"
-import ProfileUser from "@/components/profile/ProfileUser"
+import ProfileUser from "@/components/learn/profile/ProfileUser"
+import { useGetCurrentUserQuery } from "@store/slices/UserSlice"
 import { NextPage } from "next"
+import { Suspense } from "react"
 
 type Props = {}
 
 const Profile: NextPage = (props: Props) => {
+  const { data: user } = useGetCurrentUserQuery()
   return (
     <>
       {/* <TopBar backgroundColor='bg-[#58cc02]' borderColor='border-[#46a302]' /> */}
@@ -15,7 +18,11 @@ const Profile: NextPage = (props: Props) => {
 
       <div className='main-right flex justify-center gap-3 pt-14 sm:p-6 sm:pt-6 md:ml-24 c-max-tm:w-[100%] c-max-td:w-[820px]   c-min-lg:ml-64 c-min-lg:gap-12'>
         <div className='flex max-w-[592px] grow flex-col'>
-          <ProfileUser />
+          {user && (
+            <Suspense fallback={<div>loading...</div>}>
+              <ProfileUser user={user} />
+            </Suspense>
+          )}
         </div>
         <RightBar />
       </div>
