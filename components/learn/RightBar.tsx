d51ab46c3463a71e7f2ab5aa270f2b7cdc4e253c
Duraan Ali <16447314+duraanali@users.noreply.github.com>
2023-08-14 21:37:41 -0500

    profile changes

--- components/learn/RightBar.tsx
+++ components/learn/RightBar.tsx
@@ -395,34 +395,3 @@ const TreasureClosedSvg = (props: ComponentProps<"svg">) => {
   )
 }
 
-const XpProgressSection = () => {
-  const xpToday = 20
-  const goalXp = 100
-  return (
-    <article className='flex flex-col gap-5 rounded-2xl border-2 border-gray-200 p-6 font-bold text-gray-700'>
-      <div className='flex items-center justify-between'>
-        <h2 className='text-xl'>XP Progress</h2>
-      </div>
-      <div className='flex gap-5'>
-        <TreasureClosedSvg />
-        <div className='flex grow flex-col justify-around'>
-          <h3 className='font-normal text-gray-500'>Daily goal</h3>
-          <div className='flex items-center gap-5'>
-            <div className='relative h-4 w-full grow rounded-full bg-gray-200'>
-              {xpToday > 0 && (
-                <div
-                  className='absolute left-0 top-0 h-4 rounded-full bg-yellow-400'
-                  style={{ width: `${Math.min(1, xpToday / goalXp) * 100}%` }}>
-                  <div className='absolute left-2 top-1 right-2 h-[6px] rounded-full bg-yellow-300'></div>
-                </div>
-              )}
-            </div>
-            <div className='text-md shrink-0 font-normal text-gray-400'>
-              {xpToday}/{goalXp} XP
-            </div>
-          </div>
-        </div>
-      </div>
-    </article>
-  )
-}
