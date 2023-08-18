2ec302e29abf1021cbb2747aa6c36bdc765463e9
jamaalcoder <kabarojamaal@gmail.com>
2023-08-19 00:00:41 +0100

    Added:  SETTER and Past as a Props for Callback Fun

--- components/learn/profile/ProfileAddFriends.tsx
+++ components/learn/profile/ProfileAddFriends.tsx
@@ -1,10 +1,29 @@
 import LuuqadIconRightchevron from "@/components/icons/LuuqadIconRightchevron"
 import LuuqadIconWarning from "@/components/icons/LuuqadIconWarning"
 import Image from "next/image"
+import { usePathname } from "next/navigation"
+import { SetStateAction } from "react"
 
-type Props = {}
+type Props = {
+  SetModelIsOpen: React.Dispatch<SetStateAction<boolean>>
+}
 
 const ProfileAddFriends = (props: Props) => {
+  // i must use pathname to check if i am in profile page ...
+  // other wise it will throw error becuase ProfileAddFriends Component is every where ...
+  // I only Allowed the functionality of invite friend works when you are in profile page
+  const pathname = usePathname()
+  const handleAddFriend = (event: React.MouseEvent<HTMLElement>) => {
+    const checkSocial = event.currentTarget.dataset.social
+
+    if (
+      checkSocial === "social-panel-invite-friends" &&
+      pathname === "/learn/profile"
+    ) {
+      props?.SetModelIsOpen(true)
+    }
+  }
+
   const isTrue = true
   return (
     <div className='border-2 rounded-2xl'>
@@ -12,6 +31,7 @@ const ProfileAddFriends = (props: Props) => {
         Add friends
       </div>
       <div
+        onClick={handleAddFriend}
         className='flex items-center cursor-pointer p-[6px_16px]'
         data-social='social-panel-find-friends'>
         <Image
@@ -28,6 +48,7 @@ const ProfileAddFriends = (props: Props) => {
         <LuuqadIconRightchevron width={10} />
       </div>
       <div
+        onClick={handleAddFriend}
         className='flex items-center cursor-pointer p-[6px_16px]'
         data-social='social-panel-invite-friends'>
         <Image
@@ -43,6 +64,7 @@ const ProfileAddFriends = (props: Props) => {
         <LuuqadIconRightchevron width={10} />
       </div>
       <div
+        onClick={handleAddFriend}
         className='flex items-center cursor-pointer p-[6px_16px]'
         data-social='social-panel-facebook'>
         <Image
